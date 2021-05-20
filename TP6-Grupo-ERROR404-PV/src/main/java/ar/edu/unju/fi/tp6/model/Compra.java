b5ca2759fc4f39aa976df98eb0ac0c186738069b
cesarmercadook <cesarmercadook@gmail.com>
2021-05-20 13:06:20 -0300

    Creacion del repositorio y carga del proyecto a trabajar. Responsable: Mercado, Cesar David

---
+++ TP6-Grupo-ERROR404-PV/src/main/java/ar/edu/unju/fi/tp6/model/Compra.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package ar.edu.unju.fi.tp6.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Alvaro
+ *
+ */
+@Component("compraObj")
+public class Compra {
+	private int id ;
+	private Producto producto ;
+	private int cantidad ;
+
+	
+	
+	public Compra() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	/**
+	 * @param id
+	 * @param producto
+	 * @param cantidad
+	 * @param total
+	 */
+	public Compra(int id, Producto producto, int cantidad) {
+		super();
+		this.id = id;
+		this.producto = producto;
+		this.cantidad = cantidad;
+
+	}
+	@Override
+	public String toString() {
+		return "Compra [id=" + id + ", producto=" + producto + ", cantidad=" + cantidad + "]";
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the producto
+	 */
+	public Producto getProducto() {
+		return producto;
+	}
+	/**
+	 * @param producto the producto to set
+	 */
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	/**
+	 * @return the cantidad
+	 */
+	public int getCantidad() {
+		return cantidad;
+	}
+	/**
+	 * @param cantidad the cantidad to set
+	 */
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	/**
+	 * @return the total
+	 */
+	public double getTotal(double precio) {
+		double total = 0;
+		
+		total = this.cantidad * precio;
+		
+		return total;
+	}
+	
+	
+ 
+ 
+	
+
+}
